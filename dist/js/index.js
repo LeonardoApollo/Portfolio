6240b601114bb4d9089db519ad9cddf4f9014539
Mikhail Tyapkov <andrimalano3@gmail.com>
2022-09-10 16:46:58 +0300

    1st screen part2

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,14 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
-
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+hamburger.addEventListener('click', () => {
+  menu.classList.add('active');
+});
+closeElem.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
 /******/ })()
 ;
