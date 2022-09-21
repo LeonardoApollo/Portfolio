dc852bdeb2e896d5854b8c7b1d60865dc2df2057
Mikhail Tyapkov <andrimalano3@gmail.com>
2022-09-21 20:45:58 +0300

    form validation

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,19 +1,40 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
-hamburger.addEventListener('click', () => {
-  menu.classList.add('active');
-});
-closeElem.addEventListener('click', () => {
-  menu.classList.remove('active');
-});
-const counters = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
-counters.forEach((item, i) => {
-  lines[i].style.width = item.innerHTML;
+$(document).ready(function () {
+  const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close');
+  hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+  });
+  closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
+  const counters = document.querySelectorAll('.skills__ratings-counter'),
+        lines = document.querySelectorAll('.skills__ratings-line span');
+  counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+  });
+  $('form').validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 3
+      },
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      name: "Неверное имя",
+      email: {
+        required: "Введите свою почту",
+        email: "Неверный адрес почты"
+      }
+    }
+  });
 });
 /******/ })()
 ;
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,40 @@
-const hamburger = document.querySelector('.hamburger'),
+$(document).ready(function(){
+    const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
 
-closeElem.addEventListener('click', () => {
+    closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
-});
+    });
 
-const counters = document.querySelectorAll('.skills__ratings-counter'),
-    lines = document.querySelectorAll('.skills__ratings-line span');
+    const counters = document.querySelectorAll('.skills__ratings-counter'),
+        lines = document.querySelectorAll('.skills__ratings-line span');
 
-counters.forEach( (item, i) => {
-    lines[i].style.width = item.innerHTML;
+    counters.forEach( (item, i) => {
+        lines[i].style.width = item.innerHTML;
+    });
+
+		$('form').validate({
+			rules: {
+				name: {
+                    required: true,
+                    minlength: 3
+                },
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: "Неверное имя",
+				email: {
+					required: "Введите свою почту",
+					email: "Неверный адрес почты"
+				}
+			}
+		});
 });
